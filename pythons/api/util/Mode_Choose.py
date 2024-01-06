00533ae33a0a61ba01fd8a790c21c5cde2e69241
BILL-LIU301 <1409688109@qq.com>
2024-01-06 22:46:06 +0800

    01：加入梯度检测功能。目前能看到损失值收敛，但最大梯度为270+，还需进一步分析

--- pythons/api/util/Mode_Choose.py
+++ pythons/api/util/Mode_Choose.py
@@ -15,6 +15,17 @@ class ModeTrain(Thread):
         self.flag_finish = False
         self.loss = np.array([0])
         self.loss_min, self.loss_max = 1e10, 0
+        self.grad_max = 0.0
+        self.grad_max_name = ''
+
+    def read_grad_max(self, model, prefix):
+        grad_max, grad_max_name = 0.0, ''
+        for name, param in model.named_parameters():
+            if param.requires_grad:
+                if param.grad is not None:
+                    grad_max = max(grad_max, param.grad.abs().max())
+                    grad_max_name = prefix + '-' + name
+        return grad_max, grad_max_name
 
     def run(self):
         self.model.set_train()
@@ -22,10 +33,12 @@ class ModeTrain(Thread):
             for i in tqdm(range(math.floor(len(dataset_cell) / self.batch_size) + 1), desc='Batch', leave=False, ncols=80, disable=False):
                 temperature_prediction = list()
                 temperature_reference = list()
+                # 训练主过程
                 for j in tqdm(range(min(self.batch_size, len(dataset_cell) - i * self.batch_size)), desc='Seq', leave=False, ncols=80):
                     inp1 = dataset_cell[i * self.batch_size + j][1:3]
                     inp2 = dataset_cell[i * self.batch_size + j][3:]
-                    temperature_prediction.append(self.model(inp1, inp2))
+                    temp = self.model(inp1, inp2)
+                    temperature_prediction.append(temp)
                     temperature_reference.append(inp2[:, 1:])
                 loss_mean, self.loss = self.criterion(pre=temperature_prediction, ref=temperature_reference)
                 self.optimizer.zero_grad()
@@ -33,4 +46,9 @@ class ModeTrain(Thread):
                 self.optimizer.step()
                 self.loss_min = min(self.loss_min, self.loss.min())
                 self.loss_max = max(self.loss_max, self.loss.max())
+
+                # 梯度检测
+                self.grad_max, self.grad_max_name = 0.0, ''
+                self.grad_max, self.grad_max_name = self.read_grad_max(self.model.encoder, 'encoder')
+                self.grad_max, self.grad_max_name = self.read_grad_max(self.model.decoder, 'decoder')
         self.flag_finish = True
