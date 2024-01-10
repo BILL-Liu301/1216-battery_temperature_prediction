8597f082f0db14b1c2551e84a6468ca8aec6a579
hw <1409688109@qq.com>
2024-01-10 10:15:32 +0800

    02：增加了test_step以及绘图功能

--- pythons/main_PreEncoder.py
+++ pythons/main_PreEncoder.py
@@ -1,12 +1,14 @@
+import matplotlib.pyplot as plt
 import torch
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader, random_split
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 
 from api.models.pre_encoder import PreEncoder_LightningModule
 from api.datasets.pre_encoder import PreEncoder_Dataset
-from pytorch_lightning.callbacks.early_stopping import EarlyStopping
-from api.base.paras import device, paras_PreEncoder
-from api.base.paths import path_data_origin_pkl, path_pts
+from api.base.paras import device, paras_PreEncoder, num_measure_point
+from api.base.paths import path_data_origin_pkl, path_pts, path_figs_test
+from api.util.plots import plot_for_pre_encoder_test
 
 if __name__ == '__main__':
     # 模式选择
@@ -29,9 +31,18 @@ if __name__ == '__main__':
     dataset_loader_val = DataLoader(valid_set, batch_size=2, pin_memory=True, num_workers=1)
     dataset_loader_test = DataLoader(test_set, batch_size=2, pin_memory=True, num_workers=1)
 
+    if mode_switch['Train_Validation']:
+        early_stop_callback = EarlyStopping(monitor="loss_max_val", min_delta=0.001, patience=5, verbose=False, mode="min")
 
-    early_stop_callback = EarlyStopping(monitor="loss_max_val", min_delta=0.001, patience=5, verbose=False, mode="min")
-
-    trainer = pl.Trainer(log_every_n_steps=1, max_epochs=paras_PreEncoder['max_epochs'], default_root_dir=path_pts,
-                         accelerator='gpu', devices=1, callbacks=[early_stop_callback])
-    trainer.fit(model=pre_encoder_lm, train_dataloaders=dataset_loader_train, val_dataloaders=dataset_loader_val)
+        trainer = pl.Trainer(log_every_n_steps=1, max_epochs=paras_PreEncoder['max_epochs'], default_root_dir=path_pts,
+                             accelerator='gpu', devices=1, callbacks=[early_stop_callback])
+        trainer.fit(model=pre_encoder_lm, train_dataloaders=dataset_loader_train, val_dataloaders=dataset_loader_val)
+    if mode_switch['Test']:
+        trainer = pl.Trainer(default_root_dir=path_pts, accelerator='gpu', devices=1)
+        trainer.test(model=pre_encoder_lm, dataloaders=dataset_loader_test)
+        plt.figure(figsize=[20, 11.25])
+        for i in range(0, len(pre_encoder_lm.test_results), 4):
+            test_results = pre_encoder_lm.test_results[i:i+4]
+            plot_for_pre_encoder_test(test_results, num_measure_point)
+            plt.savefig(path_figs_test + f'{i}.png')
+            print(f'已保存{i}.png')
