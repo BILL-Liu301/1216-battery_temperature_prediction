fd9f491a065a05ae4612bec13f25b90337ae12e5
BILL-LIU301 <1409688109@qq.com>
2024-01-23 11:03:18 +0800

    03：更新了README的结果

--- pythons/api/base/paras.py
+++ pythons/api/base/paras.py
@@ -21,7 +21,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 paras_Prediction_Seq2Seq = {
     'num_measure_point': num_measure_point,
     'seq_history': 5,
-    'seq_predict': 100,
+    'seq_predict': 100,  # 100和600
     'seq_attention_once': 50,
     'split_length': 2,  # 取点间隔，间隔为n个数时，split_length=n+1
     'max_epochs': 100,
